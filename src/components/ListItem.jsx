a15727dfb05b9438fe3150a16e24a44e9635914e
Vinh Tran <daivinhtran.vt@gmail.com>
2016-05-24 03:08:00 -0400

    react skeleton

---
+++ src/components/ListItem.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+class ListItem extends React.Component {
+    render() {
+        return (
+            <li>
+                <h4>{this.props.ingredient}</h4>
+            </li>
+        );
+    }
+}
+module.exports = ListItem;
